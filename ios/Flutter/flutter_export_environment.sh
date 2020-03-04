5575598a457e6cb0fd195c978b9ce43aab8b66cc
Anh Tuan <atn832@gmail.com>
2020-03-04 15:43:47 +0900

    adding web-support. but fails to sign in

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/anhtuan/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anhtuan/git/elysium-mobile-client"
+export "FLUTTER_APPLICATION_PATH=/Users/anhtuan/git/elysium_mobile_client"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
