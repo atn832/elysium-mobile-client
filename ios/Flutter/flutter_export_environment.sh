6a207aee1a1f28e18807ce96ed3c6a5aa83310ab
Anh Tuan <atn832@gmail.com>
2020-11-13 19:49:12 +0900

    updated framework

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/anhtuan/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anhtuan/git/elysium-mobile-client"
+export "FLUTTER_APPLICATION_PATH=/Users/anhtuan/git/elysium_mobile_client"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/anhtuan/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.7.1"
 export "FLUTTER_BUILD_NUMBER=9"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
